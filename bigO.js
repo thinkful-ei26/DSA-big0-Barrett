ef90f03aa5e6fbba376d59cf02ea0aa7991edfc7
carpenter-js <carpenterbarrett@gmail.com>
2019-01-15 12:59:09 -0700

    finished drills

--- bigO.js
+++ bigO.js
@@ -9,9 +9,8 @@ function isEven(value){
     return false;
 }
 
-// input size doesn't effect complexity
 // complexity: 0(1)
-
+// why: input size doesn't effect complexity
 
 
 // Are you here?
@@ -27,10 +26,10 @@ function areYouHere(arr1, arr2) {
   return false;
 }
 
-// Increases as polynomial because of the nested array, but because of the 2 
-// inputs with different lengths its i*j
 //Ploynomial 0(i*j)
 // big 0: 0(1)
+// why: Increases as polynomial because of the nested array, but because of the 2 
+// inputs with different lengths its i*j
 
 
 // Doubler
@@ -42,7 +41,7 @@ function doubleArrayValues(array) {
   return array;
 }
 
-// The function loops through a single array once, so its complexity is linear
+// why: The function loops through a single array once, so its complexity is linear
 // complexity: linear 0(n)
 // big 0: 0(n)
 
@@ -57,7 +56,7 @@ function naiveSearch(array, item) {
   }
 }
 
-// Checks each item in array against item, complextiy goes up linearily with input size,
+// why: Checks each item in array against item, complextiy goes up linearily with input size,
 // best case the item is at index 0 of array
 // complexity: linear 0(n)
 // big0: 0(1)
@@ -73,7 +72,7 @@ function createPairs(arr) {
   }
 }
 
-// single array input, nested loop. Best and worst case stay as 0(n^2) because 
+// why: single array input, nested loop. Best and worst case stay as 0(n^2) because 
 // it will always be necessary to go throught the full array to complete the function
 // complexity: polynomial 0(n^2)
 // big0: 0(n^2)
@@ -107,7 +106,7 @@ function generateFib(num) {
   return result;
 }
 
-// complexity stays linear, single num input is looped amd then each itereation is compared 
+// why: complexity stays linear, single num input is looped amd then each itereation is compared 
 // to 3 constant conditions, so complexity is only dependant on size of n
 // complexity: linear 0(n)
 // big-0; 0(n)
@@ -138,7 +137,7 @@ function efficientSearch(array, item) {
   return -1;
 }
 
-// Because the input is cut in half with each iteration its complexity quickly 
+// why: Because the input is cut in half with each iteration its complexity quickly 
 // decreases despite the size of n.
 // complexity: logarithmic 0(log(n))
 // Big-0: best case is 0(1) because it could possibly match item on first try.
@@ -151,7 +150,7 @@ function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// will always return a result first try, no matter the size of n
+// why: will always return a result first try, no matter the size of n
 // complexity: constant 0(1)
 // big-0: 0(1)
 
@@ -171,9 +170,10 @@ function isPrime(n) {
   return true;
 }
 
-// the loop complexity is dependant on the size of the input
+// why: the loop complexity is dependant on the size of the input
 // complexity: linear 0(n)
 // big-0: 0(1)
+
 
 
 // ----- Recursive Solutions ------ 
@@ -205,4 +205,127 @@ function isPrime(n) {
 // why: the input could bea string that needs to be split on every index, 
 // making it linear
 
+
+// Binary Representation
+// Complexity: log 0(lon(n))
+// best case: 0(1) if input is 0 or a negative number
+// why: Each recursive call significantly reduces the size of the input
+
+
+// Factorial
+// Complexity: linear 0(n) 
+// best case: 0(1), if input is 1 or 0
+// why: each recursive call is made with n-1 until n is zero so,
+// the function runs however many times as the input number
+
+
+// Fibonacci
+// Complexity: Exponential 0(2^n)
+// best case: 0(1)
+// why: Every recursive call requires the previous 2 fib numbers, which quickly
+//  builds exponentially.
+
+
+// Anagram
+// Complexity: factorial 0(n!)
+// best case: 0(1)
+// why: Complexity gets out of hand quickly. The loop runs through every possible
+// combination.
+
+
+// Animal Hierarchy 
+// Complexity: linear 0(n * n)
+// best case: 
+// why: The .filter decreases the complexity so that each loop is 0(n)
+
+
+// Organization chart
+
+function traverseB(node, indent=0) {
+  for (var key in node) {
+    console.log(' '.repeat(indent), key);
+    traverseB(node[key], indent + 4);
+  }
+}
+
+// Complexity: Linear 0(n)
+// best case:
+// whay: The function is called recursively once for each part of 'node'
+
+
+
+// ----- Iterative Solutions ------ 
+
+// Countin Sheep
+
+function countSheepLoop(num){
+  for(let i=num; i>0; i--){
+    console.log(`counting sheeps ${i}`);
+  }
+}
+countSheepLoop(10);
+
+// Complexity: Linear 0(n)
+// Why: The loop runs once for each num
+
+// Doubler
+
+function double_all(arr) {
+  var ret = Array(arr.length);
+  for (var i = 0; i < arr.length; ++i) {
+    ret[i] = arr[i] * 2;
+  }
+  return ret;
+}
+let arr = [10,4,5,2,1];
+console.log(double_all(arr));
+
+// Complexity: Linear 0(n)
+// Function hits each index of input array once
+
+// String Reverse
+
+function reverse_tail(str) {
+  var accumulator = '';
+  while (str !== '') {
+    accumulator = str[0] + accumulator;
+    str = str.slice(1);
+  }
+  return accumulator;
+}
+
+// Complexity: Linear 0(n)
+// Why: reduces string by one with each loop and stops when string length is 0
+
+
+// Triangular
+
+function triangle(n) {
+  var tot = 0;
+  for (var i = 1; i <= n; ++i) {
+    tot += n;
+  }
+  return tot;
+}
+
+// Complexity: linear 0(n)
+// Why: The function loops through n once for each iteration
+
+// String splitter
+
+function split(str, sep) {
+  var ret = [];
+  while (true) {
+    var idx = str.indexOf(sep);
+    if (idx == -1) break;
+    ret.push(str.slice(0, idx));
+    str = str.slice(idx + sep.length);
+  }
+  ret.push(str);
+  return ret;
+}
+
+// Complexity: linear 0(n)
+// Why: The number of instances of sep in str determines directly
+// the number of iterations
 
